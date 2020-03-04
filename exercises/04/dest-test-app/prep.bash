1d6eb5ee4b98986dbef466e902ca32bb83348a14
DJ Adams <dj.adams@pobox.com>
2020-03-04 08:41:34 +0000

    update app archive and additions to prep script

--- exercises/04/dest-test-app/prep.bash
+++ exercises/04/dest-test-app/prep.bash
@@ -6,6 +6,15 @@
 # - copies the archive and manifest to the accessible Chrome OS
 #   download folder, ready for upload via the Cockpit
 
+# Then uses 'cf push' to deploy directly.
+
+appname=$(yq r manifest.yml applications[0].name)
+echo CF app '${appname}'
+
 rm app.zip
 zip app.zip .npmrc package.json xs-app.json
 cp manifest.yml app.zip ${HOME}/Downloads/
+
+cf d ${appname} -f
+cf push
+
